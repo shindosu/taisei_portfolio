7cc012577361cd73271b90fe463c02c5118a9f56
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-12-10 15:07:31 -1000

    edited controller and made incomplete seed file

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,14 @@
 class ProjectsController < ApplicationController
   def index
+    if params[:query].present?
+      sql_query = "title ILIKE :query OR skill ILIKE :query"
+      @projects = @projects.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @projects = Project.all.order("LOWER(title)")
+    end
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 end
