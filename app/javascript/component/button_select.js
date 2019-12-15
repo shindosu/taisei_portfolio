f70869404e811c71ae212bd7de045d4e418d5335
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-12-15 19:50:04 +0900

    button link filters successfully

--- app/javascript/component/button_select.js
+++ app/javascript/component/button_select.js
@@ -1,11 +1,13 @@
-const button = document.querySelectorAll('.btn');
-const button_array = [button[0], button[1], button[2]];
-const handleClick = (event) => {
+if (window.location.href.includes("projects")){
+  const button = document.querySelectorAll('.btn');
+  const button_array = [button[0], button[1], button[2]];
+  const handleClick = (event) => {
+    button_array.forEach(element => {
+      element.classList.remove('btn-danger');     
+    });
+    event.currentTarget.classList.add('btn-danger'); 
+  };
   button_array.forEach(element => {
-    element.classList.remove('btn-danger');     
-  });
-  event.currentTarget.classList.add('btn-danger'); 
-};
-button_array.forEach(element => {
-  element.addEventListener("click", (handleClick))
-});
+    element.addEventListener("click", (handleClick))
+  });  
+}
