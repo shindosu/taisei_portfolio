8157a68198e976ca57d0fc206fd67a0cbc15d7cf
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-12-18 17:39:02 +0900

    changed smtp rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,16 +72,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors= true
-
-  ActionMailer::Base.sendmail_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV["GMAIL_EMAIL"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-
   # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,8 @@
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  user_name: ENV['GMAIL_EMAIL'],
-  password: ENV['GMAIL_APP_PASSWORD'],
-  authentication: 'plain',
-  :openssl_verify_mode  => 'none',
-  enable_starttls_auto: true
-}
+ActionMailer::Base.sendmail_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV["GMAIL_EMAIL"],
+  password:             ENV["GMAIL_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
