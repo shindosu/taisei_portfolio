0a77e99c08c1f941a37aa9f8fdf6c3136f3b5b48
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-12-19 00:23:46 +0900

    changed domain name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,20 +31,19 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   #send email in dev mode
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors= true
 
-  # ActionMailer::Base.sendmail_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gmail.com',
-  #   user_name:            ENV["GMAIL_EMAIL"],
-  #   password:             ENV["GMAIL_PASSWORD"],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
+  ActionMailer::Base.sendmail_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV["GMAIL_EMAIL"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   config.action_view.embed_authenticity_token_in_remote_forms = true
 
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
-ActionMailer::Base.smtp_settings = {
+ActionMailer::Base.sendmail_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'gmail.com',
+  domain:               'www.taiseiyamada.com',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
