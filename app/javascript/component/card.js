2299f101c56f09405f036039e5e434877e0d4542
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2019-12-22 22:11:03 +0900

    got rid of opacity in mobile

--- app/javascript/component/card.js
+++ app/javascript/component/card.js
@@ -1,11 +1,16 @@
 const changeOpacity = () => {
+
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
   if (window.location.href.includes("/projects")) {
     const card = document.querySelectorAll('.card');
     const card_array = [card[0], card[1], card[2]];
   
     card_array.forEach(element => {
       element.addEventListener("mouseover", (event) => {
-        element.querySelector(".card-image").style.opacity = 0.04;
+        if(viewportWidth < 400){
+          element.querySelector(".card-image").style.opacity = 0.04;
+        } 
         element.querySelector(".card-description").classList.remove('d-none');
         element.querySelector(".card-buttonholder").classList.remove('d-none');
         element.querySelector(".card-title").style.top = "10%";
